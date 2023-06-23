8c68d27a15e011aef87a03a0daf753ae6a62f849
Uporov Aleksei <uporovalekseii@yandex.ru>
2023-06-23 16:29:48 +0500

    -

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <script setup>
 import MainVue from '@/layouts/Main.vue'
-import { ref } from 'vue'
-
 const title = ref('Открытый университет')
-const viewport = 'width=device-width, initial-scale=1.0'
+onMounted(() => {
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+  if (isMobile && window.innerWidth <= 425) {
+    const metaViewport = document.querySelector('meta[name="viewport"]')
+    metaViewport.setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0')
+  }
+})
 </script>
 
 <template>
   <Head>
     <Title>{{ title }}</Title>
     <Meta name="description" :content="title" />
-    <Meta name="viewport" :content="viewport"/>
   </Head>
   <MainVue>
     <OpenUniversity />
