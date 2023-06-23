6161305adddc54e5531fb9d6b66e611656460d15
Uporov Aleksei <uporovalekseii@yandex.ru>
2023-06-23 16:02:47 +0500

    -

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,16 @@
 <script setup>
 import MainVue from '@/layouts/Main.vue'
+import { ref } from 'vue'
+
 const title = ref('Открытый университет')
+const viewport = 'width=device-width, initial-scale=1.0'
 </script>
 
 <template>
   <Head>
     <Title>{{ title }}</Title>
     <Meta name="description" :content="title" />
+    <Meta name="viewport" :content="viewport"/>
   </Head>
   <MainVue>
     <OpenUniversity />
@@ -17,4 +21,5 @@ const title = ref('Открытый университет')
     <UniversityPersons />
   </MainVue>
 </template>
+
 <style lang="scss"></style>
