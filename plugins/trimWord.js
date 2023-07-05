e436bc31e09e1e912777e28632d4946d6d94002c
Aleksei <uporovalekseii@yandex.ru>
2023-07-05 13:26:30 +0500

    -

--- plugins/trimWord.js
+++ plugins/trimWord.js
@@ -1,14 +1,19 @@
 
 export default defineNuxtPlugin((text, isTitle) => {
     try {
-        const words = text.split(' ');
-        const maxWords = isTitle ? 3 : 4;
-      
-        if (words.length <= maxWords) {
-          return text;
-        } else {
-          return words.slice(0, maxWords).join(' ') + '...';
+      const words = text.split(' ');
+      const maxWords = isTitle ? 3 : 5;
+      const punctuationMarks = ['.', ',', '!', '?', ':', ';'];
+      if (words.length <= maxWords) {
+        return text;
+      } else {
+        let truncatedText = words.slice(0, maxWords).join(' ');
+        const lastCharacter = truncatedText[truncatedText.length - 1];
+        if (punctuationMarks.includes(lastCharacter)) {
+          truncatedText = truncatedText.slice(0, -1);
         }
+        return truncatedText + '...';
+      }
     } catch {
       return text
     }
